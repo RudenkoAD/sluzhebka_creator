f084c17bcd1959db03d01e09aaa0cc15bd732259
Shlyakhtun Prokhor <shlyakhtun.prohor@gmail.com>
2023-11-16 16:42:40 +0300

    Added frontend inputs

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import axios from "axios";
 
 import "bootstrap/scss/bootstrap.scss";
+import { InputGroup } from "./components/input-group/input-group";
 
 const root = createRoot(document.getElementById("app")!);
 root.render(
@@ -9,14 +10,22 @@ root.render(
     <h1>Welcome to our website</h1>
 
     <div className="container">
-      <form>
+      <form method="post">
         <select className="form-select" aria-label="Default select example">
-          <option defaultValue="selected">Open this select menu</option>
           <option value="club6">Клуб шестерки</option>
         </select>
 
+        <InputGroup placeholder="Группа" />
+        <InputGroup placeholder="ФИО проводящего в родильном падеже" />
+        <InputGroup placeholder="ФИО проводящего" />
+        <InputGroup placeholder="Дата" type="date" />
+        <InputGroup placeholder="Время начала" type="time" />
+        <InputGroup placeholder="Время окончания" type="time" />
+        <InputGroup placeholder="Количество людей" type="number" />
+        <InputGroup placeholder="Название мероприятия" />
+
         <button type="submit" className="btn btn-primary mt-2">
-          Submit
+          Скачать служебку
         </button>
       </form>
     </div>
